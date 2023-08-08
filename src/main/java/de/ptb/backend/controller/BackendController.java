0149361652bba7f2da5486001a35ccd02a8021b6
hoffma29 <tobias.hoffmann@ptb.de>
2023-08-08 15:20:28 +0200

    Update(Backend): Add License to all files

--- src/main/java/de/ptb/backend/controller/BackendController.java
+++ src/main/java/de/ptb/backend/controller/BackendController.java
@@ -1,5 +1,20 @@
+/*
+Copyright (c) 2023 Physikalisch-Technische Bundesanstalt (PTB), all rights reserved.
+This source code and software is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, version 3 of the License.
+The software is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with this XSD.  If not, see http://www.gnu.org/licenses.
+CONTACT: 		info@ptb.de
+DEVELOPMENT:	https://d-si.ptb.de
+AUTHORS:		Wafa El Jaoua, Tobias Hoffmann, Clifford Brown, Daniel Hutzschenreuter
+LAST MODIFIED:	2023-08-08
+*/
 package de.ptb.backend.controller;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import de.ptb.backend.BERT.*;
 import de.ptb.backend.model.DKCRRequestMessage;
@@ -16,7 +31,6 @@ import de.ptb.backend.service.PidReportFileSystemWriteService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
